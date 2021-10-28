3e85a33b96c398eefab48c2f7a9c80a9db8c98e7
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-10-28 16:46:01 +0300

    save changes of task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,18 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
-    // Reject
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
   }
 }
+const firstDelay = document.querySelector('[name="delay"]')
+const delayStep = document.querySelector('[name="step"]')
+const amount = document.querySelector('[name="amount"]')
+const createPromissesBtn = document.querySelector('[type="submit"]')
+
+createPromissesBtn.addEventListener('click', createPromise)
