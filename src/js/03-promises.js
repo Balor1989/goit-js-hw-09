9ef5d982da5831c147158a29efdc17ea01c34d09
Evgeniy Grishko <evgeniygrish@gmail.com>
2021-10-28 21:17:22 +0300

    add a solution of task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,38 +1,46 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-
-
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    resolve(position, delay);
-  } else {
-  reject (position, delay);
-  }
-}
 const refs = {
-form: document.querySelector('.form'),
-firstDelay: document.querySelector('[name="delay"]'),
-delayStep: document.querySelector('[name="step"]'),
-amount: document.querySelector('[name="amount"]'),
-createPromissesBtn: document.querySelector('[type="submit"]')
+formForInputValues: document.querySelector('.form'),
+inputValueOfFirstDelay: document.querySelector('[name="delay"]'),
+inputValueOfDelayStep: document.querySelector('[name="step"]'),
+inputValueOfAmount: document.querySelector('[name="amount"]'),
 }
 
+const createPromise = (position, delay) => {
+  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+        return
+      }
+        reject({ position, delay })
+    }, delay)
+  })
+}
 
 const onFormSubmit = (e) => {
   e.preventDefault();
-
-  
-  console.log(refs.firstDelay.value)
-  console.log(refs.delayStep.value)
-  console.log(refs.amount.value)
-
-  e.currentTarget.reset()
-
- 
-
-
+  const referral = {
+  delay: Number(refs.inputValueOfFirstDelay.value),
+  delayStep: Number(refs.inputValueOfDelayStep.value),
+  amount: Number(refs.inputValueOfAmount.value),
 }
 
-refs.form.addEventListener('submit', onFormSubmit)
+  for (let i = 1; i <= referral.amount; i += 1) {
+    createPromise(i, referral.delay)
+      .then(({ position, delay }) => {
+        Notify.success(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
+    referral.delay += referral.delayStep;
+  }
+
+  e.currentTarget.reset()
+}
+
+  refs.formForInputValues.addEventListener('submit', onFormSubmit)
 
